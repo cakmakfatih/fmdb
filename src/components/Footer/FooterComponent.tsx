63f71879080006c99c3de25a11ddde5f9134a845
Fatih Cakmak <cakmakfatih@outlook.com.tr>
2024-06-19 22:36:24 +0300

    fixed spaces in Footer text

--- src/components/Footer/FooterComponent.tsx
+++ src/components/Footer/FooterComponent.tsx
@@ -1,6 +1,6 @@
 export function FooterComponent() {
   return (
-    <footer className="mt-4 bg-black border-t border-gray-700 text-white flex flex-col items-center p-6 text-center">
+    <footer className="mt-16 bg-black border-t border-gray-700 text-white flex flex-col items-center p-6 text-center">
       <h1 className="text-2xl">Created by Fatih Cakmak @ 2024</h1>
       <span className="text-sm">
         It's an open source project that you can clone and run yourself, here is
@@ -16,14 +16,14 @@ export function FooterComponent() {
       <div className="h-8"></div>
       <p className="text-sm text-white/[0.64]">
         Data used in the project is coming from the TMDB API, special thanks to
-        the all TMDB team. You can check out
+        the all TMDB team. You can check out{" "}
         <a
           className="underline opacity-75 hover:opacity-100 hover:text-white"
           target="_blank"
           href="https://www.themoviedb.org/"
         >
           TMDB API
-        </a>
+        </a>{" "}
         from here.
       </p>
     </footer>
